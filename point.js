18949245acce53e4310160e422f918b5d3754566
Pascal Sommer <p@pascalsommer.ch>
2017-01-19 13:37:26 +0100

    polar coords point lib

--- point.js
+++ point.js
@@ -17,18 +17,29 @@ function Point(r, t){
 	this.scale = function(fac){
 		var new_r = this.r*fac;
 		var new_theta = this.t;
-		return new Point(this.r * r, this.t);
+		return new Point(new_r, new_theta);
 	}
 	this.sub = function(p){
 		return this.add(p.scale(-1));
 	}
 	this.add = function(p){
-		var r;
+		var sq_r, r;
 		var t;
-		var alpha = p.t - this.t;
+		var alpha = Math.PI - (p.t - this.t);
 
-		r = (this.r ** 2) + (p.r ** 2) - (Math.cos(alpha) * this.r * p.r);
-		t = this.t + 42; //todo
+		var sq_tr, sq_pr;
+
+		sq_tr = this.r * this.r;
+		sq_pr = p.r * p.r;
+
+		sq_r = sq_tr + sq_pr - 2*Math.cos(alpha)*this.r*p.r;
+
+		r = Math.sqrt(sq_r);
+		t = this.t + Math.acos((sq_r + sq_tr - sq_pr)/(2*r*this.r));
+
+		if (t!==t){ // t is NaN, silly JS...
+			t = 0;
+		}
 
 		return new Point(r, t);
 	}
